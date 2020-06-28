3747541c4a5ae50907c63e92a140471f1b3fa22d
Divino Rodrigues <divino.sr@hotmail.com>
2020-06-27 22:21:31 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Versão Incial'),
+      ),
+    );
+  }
+}

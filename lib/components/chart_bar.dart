935b36f14daefecb2776ce66124cd6669241b663
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-02 23:40:52 -0300

    Improving the layout of the chart

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -11,9 +11,12 @@ class CHartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          child: Text(
-            '${value.toStringAsFixed(2)}',
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text(
+              '${value.toStringAsFixed(2)}',
+            ),
           ),
         ),
         SizedBox(height: 5),
